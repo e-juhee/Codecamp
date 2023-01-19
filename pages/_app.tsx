e84dd3334c4f0bf69959ee07f076813900003c52
e-juhee <juhee971204@naver.com>
2023-01-19 21:36:21 +0900

    add tsconfig comment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,18 @@
 // import '../styles/globals.css'
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
-import { AppProps } from 'next/app'
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { AppProps } from "next/app";
 
-function MyApp({ Component }:AppProps) {
-
+function MyApp({ Component }: AppProps) {
   const client = new ApolloClient({
     uri: "http://backend-example.codebootcamp.co.kr/graphql",
     cache: new InMemoryCache(),
-  })
+  });
 
   return (
     <ApolloProvider client={client}>
       <Component />
     </ApolloProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
