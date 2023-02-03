a02ece00f3696104adc4e71f9bb4b1448112a19e
e-juhee <juhee971204@naver.com>
2023-02-03 17:21:10 +0900

    firebase

---
+++ src/commons/libraries/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCn2ehn3VWBcK0uo1hTUDjUVp3T34TDpg4",
+  authDomain: "ejuhee-de7c7.firebaseapp.com",
+  projectId: "ejuhee-de7c7",
+  storageBucket: "ejuhee-de7c7.appspot.com",
+  messagingSenderId: "480441208370",
+  appId: "1:480441208370:web:0a72c9bb90d6f0869c762b",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
